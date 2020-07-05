9a5cd345709dd76213f5805780d7636e265437d1
Cemalettin Taş <cemalettin.tas.metu@gmail.com>
2020-07-05 20:56:43 +0300

    working example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
-import React from 'react'
-import 'main.scss';
-import './App.scss'
+import React, { useState, useRef, useCallback } from 'react'
+import 'main.scss'
+import 'App.scss'
+import useInfiniteScroller from 'utils/useInfiniteScroller/useInfiniteScroller'
 
-function App() {
-  return <div className="App"></div>
+// // https://pixabay.com/api/
+// const baseUrl = 'https://pixabay.com/api/?'
+
+// const makeQuery = (query, pageNumber = 1) =>
+//   `${baseUrl}key=${process.env.REACT_APP_PIXA_KEY}&q=${query}&page=${pageNumber}`
+
+export default function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+
+  const { images, hasMore, loading, error } = useInfiniteScroller(
+    query,
+    pageNumber,
+  )
+
+  const observer = useRef()
+  const lastElementRef = useCallback(
+    (node) => {
+      if (loading) return
+      if (observer.current) observer.current.disconnect()
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1)
+        }
+      })
+      if (node) observer.current.observe(node)
+    },
+    [loading, hasMore],
+  )
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  return (
+    <>
+      <input type="text" value={query} onChange={handleSearch}></input>
+      {images.map((image, index) => {
+        if (images.length === index + 1) {
+          return (
+            <img
+              ref={lastElementRef}
+              src={image}
+              key={image}
+              alt="preview"
+            ></img>
+          )
+        }
+        return <img src={image} key={image} alt="preview"></img>
+      })}
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
+    </>
+  )
 }
-
-export default App
---
+++ src/utils/useInfiniteScroller/useInfiniteScroller.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function useInfiniteScroller(query, pageNumber) {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [images, setImages] = useState([])
+  const [hasMore, setHasMore] = useState(false)
+
+  useEffect(() => {
+    setImages([])
+  }, [query])
+
+  useEffect(() => {
+    let cancel
+    if (query === '') {
+      console.log('query empty, returning')
+      return () => {
+        cancel = true
+      }
+    }
+    setLoading(true)
+    setError(false)
+    console.log('request sending')
+    axios({
+      method: 'GET',
+      url: `https://pixabay.com/api/?key=17300653-639be655694062aab8cd3f5ab&q=${query}&page=${pageNumber}`,
+      cancelToken: new axios.CancelToken((c) => {
+        cancel = c
+      }),
+    })
+      .then((res) => {
+        setImages((prevImages) => [
+          ...new Set([
+            ...prevImages,
+            ...res.data.hits.map((b) => b.previewURL),
+          ]),
+        ])
+        setHasMore(res.data.hits.length > 0)
+        setLoading(false)
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return
+        setError(true)
+        console.log(e)
+      })
+    return () => cancel()
+  }, [query, pageNumber])
+
+  return { loading, error, images, hasMore }
+}
+
+export default useInfiniteScroller
